f2a1e3fe89982be8362a46ad60d4d82992d70a02
Verta-master <vertyagina@gmail.com>
2020-08-11 12:31:16 +0600

    Menu

--- js/script.js
+++
@@ -1,9 +0,0 @@
-//Mobile menu
-$('.menu__btn').click(function() {
-  $(this).toggleClass('menu__btn--cross');
-  $('.menu__list').slideToggle();
-});
-
-$('.menu__dropbtn').click(function() {
-  $('.menu__sublist').slideToggle();
-});
